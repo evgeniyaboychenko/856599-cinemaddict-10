ab1786fb8de23cf87f31651daf36cd620532e2a0
evgeniyaboychenko <evgeniyaboychenko@gmail.com>
2020-02-01 20:25:10 +0700

    исправление замечаний

--- src/components/sort.js
+++ src/components/sort.js
@@ -9,9 +9,9 @@ const SortType = {
 const createSortFilmTemplate = () => {
   return (
     `<ul class="sort">
-       <li><a href="#" data-sort-type="${SortType.DEFAULT}" class="sort__button sort__button--active">Sort by default</a></li>
-       <li><a href="#" data-sort-type="${SortType.DATE}" class="sort__button">Sort by date</a></li>
-       <li><a href="#" data-sort-type="${SortType.RATING}" class="sort__button">Sort by rating</a></li>
+       <li><a href="#" data-sort-type="${SortType.DEFAULT}" class="sort__button sort__button--active">${SortType.DEFAULT}</a></li>
+       <li><a href="#" data-sort-type="${SortType.DATE}" class="sort__button">${SortType.DATE}</a></li>
+       <li><a href="#" data-sort-type="${SortType.RATING}" class="sort__button">${SortType.RATING}</a></li>
     </ul>`
   );
 };
